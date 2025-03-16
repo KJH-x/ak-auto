ec0c4fc65f23c1de2a87444fce2f55b5ea5a41eb
KJH-x <53437291+KJH-x@users.noreply.github.com>
2025-03-16 21:35:12 +0800

    FIx bug: provide excute dir when call batch from maa

--- _load_MaaReport_batch.py
+++ _load_MaaReport_batch.py
@@ -1,20 +1,30 @@
 import os
 import sys
+from typing import List
 
 import MaaReport
+from config_manager import ConfigManager
 
-batch_location = MaaReport.__file__
+batch_path = MaaReport.__file__
 os.chdir(sys.path[0])
 
+common_config_path = 'config/common.json'
 
-for i in range(1, 6):
+common_config = ConfigManager(common_config_path)
+maa_config = common_config.get_launcher_maas_config()
+maa_instances: List[str] = maa_config["maa_instances"]
+instance_count: int = len(maa_instances)
+work_dir = os.path.dirname(batch_path)
+
+for i in range(1, instance_count+1):
 
     start_batch_content = "\n".join([
         "@echo off>nul",
         "chcp 65001>nul",
+        f"cd {work_dir}",
         " ".join([
             # "@REM",
-            sys.executable, batch_location,
+            sys.executable, batch_path,
             "--action", '"startUpRecord"',
             "--group", str(i),
             "--message", '"任务开始"'
@@ -24,9 +34,10 @@ for i in range(1, 6):
     finish_batch_content = "\n".join([
         "@echo off>nul",
         "chcp 65001>nul",
+        f"cd {work_dir}",
         " ".join([
             # "@REM",
-            sys.executable, batch_location,
+            sys.executable, batch_path,
             "--action", '"finishKillReport"',
             "--group", str(i),
             "--message", '"任务完成"',
